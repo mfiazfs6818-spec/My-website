1cf7a9ca623c51608ce7c37fe93ec2be0f465811
mfiazfs6818-spec <mfiazfs6818@gmail.com>
2025-09-30 15:12:24 +0500

    Update index.html with new design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Site is working!");
+// Button Alert
+document.getElementById("learnBtn").addEventListener("click", function() {
+  alert("Thanks for visiting! More content coming soon 😍");
+});
+
+// Contact Form
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Your message has been sent successfully ✅");
+});
